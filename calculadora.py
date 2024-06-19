efa2bae77810f77a3bab82d3104d6a69257d6747
RuParada1999 <ru.parada@duocuc.cl>
2024-06-19 15:31:36 -0400

    Nuevas funciones
    Se añadio la funcion de restar y dividir

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,11 @@
 #hola este es el primer dialogo
 
 #hola este es el segundo dialogo
+
+def calcularResta (num1, num2):
+    resultado = num1 - num2
+    return resultado
+
+def calcularDivision (num1, num2):
+    resultado = num1 / num2
+    return resultado
