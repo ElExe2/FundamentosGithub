5642dfac5aa9784aa9251140f58031ce636d5a2b
RuParada1999 <ru.parada@duocuc.cl>
2024-06-19 15:50:45 -0400

    Nuevas funciones y cambios

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,4 @@
 #hola este es el primer dialogo
-def multiplicacion (num1,num2):
-    multi=num1*num2
-    return multi
 
 def suma (num1,num2):
     sum=num1+num2
@@ -11,10 +8,18 @@ def calcularResta (num1, num2):
     resultado = num1 - num2
     return resultado
 
+def multiplicacion (num1,num2):
+    multi=num1*num2
+    return multi
+
 def calcularDivision (num1, num2):
     resultado = num1 / num2
     return resultado
 
 while True:
     op=(int(input("ingrese la opcion que mas le corresponda\n1.suma\n2.resta\n3.division\n4.multiplicacion\n")))
-
+    
+    if op == 1:
+        num1 = int(input('Ingrese el primer numero:\n'))
+        num2= int(input('Ingrese el segundo numero:\n'))
+        print(suma(num1, num2))
