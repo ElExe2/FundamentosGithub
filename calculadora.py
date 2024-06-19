248a68f7923f41b53b2fd4ab5523f5ccb5cbfbac
ElExe2 <ez.andana@duocuc.cl>
2024-06-19 15:34:06 -0400

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,13 @@
 #hola este es el primer dialogo
+def multiplicacion (num1,num2):
+    multi=num1*num2
+    return multi
+
+def suma (num1,num2):
+    sum=num1+num2
+    return sum
+
+while True:
+    op=(int(input("ingrese la opcion que mas le corresponda\n1.suma\n2.resta\n3.division\n4.multiplicacion\n")))
 
 #hola este es el segundo dialogo
